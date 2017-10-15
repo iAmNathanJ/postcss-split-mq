95e974e3646451d2e9def6cf5a7cdcff44b9463a
iAmNathanJ <jacobs@heysparkbox.com>
2017-10-15 13:46:56 -0400

    Reorg: Move lib/ into src/
    - compile lib with build step
    - eliminate util.promisify for older node version support

---
+++ src/lib/io.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { readFile, writeFile } from 'fs';
+
+export const read = (filePath) => {
+  return new Promise((resolve, reject) => {
+    readFile(filePath, 'utf-8', (err, fileContents) => {
+      if (err) reject(err);
+      resolve(fileContents);
+    });
+  });
+};
+
+export const write = (filePath, fileContents) => {
+  return new Promise((resolve, reject) => {
+    writeFile(filePath, fileContents, (err) => {
+      if (err) reject(err);
+      resolve(fileContents);
+    });
+  });
+};
