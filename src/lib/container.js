26a8dabcc54e95a196d334ba9ecde40162924460
iAmNathanJ <jacobs@heysparkbox.com>
2017-10-16 22:57:57 -0400

    fix: properly check for already found regex

--- src/lib/container.js
+++ src/lib/container.js
@@ -6,14 +6,20 @@ export const createContainer = option => {
 
 export const createUpdaterFn = containers => atRule => {
   const killRules = [];
+
   containers.forEach(container => {
     container.match.forEach(regex => {
-      const previouslyFound = false;
+      const previouslyFound = findRegex(regex, killRules);
       if (!previouslyFound && regex.test(atRule.params)) {
         container.result.append(atRule.clone());
         killRules.push(atRule);
       }
     });
   });
+
   killRules.forEach(rule => rule.remove());
+};
+
+export const findRegex = (regex, arr) => {
+  return arr.find(elem => regex.toString() === elem.toString());
 };
